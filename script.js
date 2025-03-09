3ebae3a8dea7e97b2ad51b5e4253058715614f20
Charger <118035373+Eliaspierce@users.noreply.github.com>
2025-03-09 23:22:32 +0930

    Update script.js
    4 new line
    + 7 line
    = 11 total line

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function getWeather() {
 
     const apiKey = eca6e221658fbe29fc36881b1e629674;
     const city = document.getElementById('city').value;
-    
+
+    if (!city) {
+        alert('Please enter a city')
+        return;
+    }
 }
